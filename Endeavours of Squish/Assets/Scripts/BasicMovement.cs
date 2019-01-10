7fec55da9c4f14f124b390b84ea834e048766d08
Cameron August <31994434+cameronaugust@users.noreply.github.com>
2019-01-09 19:13:11 -0800

    Added movement to player object
    Used physics rather then translations for object for smoother movement in a space like enviroment.

---
+++ Endeavours of Squish/Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,29 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    public float moveHoriztonal;
+    public float moveVertical;
+    public float speed;
+
+    public Rigidbody2D player;
+    private Vector2 movement; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveHoriztonal = Input.GetAxis("Horizontal") * speed;
+        moveVertical = Input.GetAxis("Vertical") * speed;
+
+        movement = new Vector2(moveHoriztonal, moveVertical);
+        player.velocity = movement * speed;
+    }
+}
