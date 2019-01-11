7bbc1bbba9d0ad34860d94586b0c4ef908c30ee2
Cameron August <31994434+cameronaugust@users.noreply.github.com>
2019-01-11 12:41:21 -0800

    Added Projectiles
    Fun!

---
+++ Endeavours of Squish/Assets/Scripts/KUTIEPIE.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/* 1/11/2019 CJA
+ * Weapon Class for three shooting points
+ */
+public class KUTIEPIE : MonoBehaviour
+{
+    //points to transform
+    public Transform centerShooter;
+    public Transform SideShooterLeft;
+    public Transform SideShooterRight;
+
+    public GameObject bulletPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //looks for the Fire1 button pre-assigned by Unity
+        if (Input.GetButtonDown("Fire1"))
+        {
+            //calls method Shoot() when player clicks
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        // shooting logic
+       Instantiate(bulletPrefab, SideShooterLeft.position, SideShooterLeft.rotation);
+
+        Instantiate(bulletPrefab, centerShooter.position, centerShooter.rotation);
+
+       Instantiate(bulletPrefab, SideShooterRight.position, SideShooterRight.rotation);
+    }
+}
