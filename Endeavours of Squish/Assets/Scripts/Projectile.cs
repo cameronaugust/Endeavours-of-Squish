a1ae5571e8d9065c29a0e6867e538714b2b80cb7
Cameron August <31994434+cameronaugust@users.noreply.github.com>
2019-01-16 21:38:38 -0800

    Planet Projectile Logic
    Added in scripts that control enemy planets projectiles

--- Endeavours of Squish/Assets/Scripts/Projectile.cs
+++ Endeavours of Squish/Assets/Scripts/Projectile.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
+public class Projectile : MonoBehaviour
 {
     public float speed = 20f;
     public int damage = 40;
