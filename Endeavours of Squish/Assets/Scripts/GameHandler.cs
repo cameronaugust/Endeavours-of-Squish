e3ca8c89b54021681485d3f6314c57bf791352ad
Bryan <ongbryan71@yahoo.com>
2019-01-19 17:34:16 -0800

    Started Health System

---
+++ Endeavours of Squish/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        HealthSystem healthSystem = new HealthSystem(100);
+
+        Debug.Log("Health; " + healthSystem.GetHealth());
+
+        CMDebug.ButtonUI(new Vector2(100,100), "damage", () =>
+
+    }
+
+}
+  
